2fe4dbca6db80593ec9f997284455be6a753107f
seonwooke <seonookkim@gmail.com>
2023-04-25 13:48:17 +0900

    :art: Redux-Provider Structure
    Redux-Provider Structure

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -19,11 +20,13 @@ store.dispatch({
 
 const render = () => root.render(
   <React.StrictMode>
-    <App
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />
+    <Provider store={store}>
+      <App
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+      />
+    </Provider>
   </React.StrictMode>
 );
 render();
