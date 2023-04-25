c31949144221303e1d3a68852c712112980cb4ba
seonwooke <seonookkim@gmail.com>
2023-04-25 13:21:38 +0900

    :zap: Combine reducer
    Combine reducer

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import counter from './reducers';
+import rootReducer from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(counter);
+const store = createStore(rootReducer);
+
+store.dispatch({
+  type: "ADD_TODO",
+  text: "USER REDUX"
+})
 
 const render = () => root.render(
   <React.StrictMode>
