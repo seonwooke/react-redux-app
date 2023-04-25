8c36915220c72133cc2c8c34a8bcee89970f6eb3
seonwooke <seonookkim@gmail.com>
2023-04-25 13:12:15 +0900

    :art: Counter App
    Counter App

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+type Props = {
+    value: number;
+    onIncrement: () => void;
+    onDecrement: () => void;
+}
+
+function App({ value, onIncrement, onDecrement }: Props) {
+    return (
+        <div className="App">
+            Clicked:  {value} times
+            <button onClick={onIncrement}>
+                +
+            </button>
+            <button onClick={onDecrement}>
+                -
+            </button>
+        </div>
+    );
+}
+
+export default App;
